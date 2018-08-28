c960a2d05bccbfa2ec5527923cf9a5894c493c1c
dylan orgeron <dylan.orgeron@gmail.com>
2018-08-28 10:29:46 -0500

    refactor

--- scripts/index.ts
+++ scripts/index.ts
@@ -3,6 +3,14 @@ import $ from 'jquery'
 class Odometer{
 	public digits:number[] = []
 	public digitPositions:number[] = []
+	private digitsToHide = this.startValue.toString().length - this.endValue.toString().length
+	private scrollDirection:string = this.startValue> this.endValue ? 'down' : 'up'
+	private increment = this.scrollDirection === 'down' ? -1 : 1
+	private targetSpeed = 50
+	// set in constructor
+	private height = 0
+	private top = 0
+	private cutoff = 0
 
 	constructor(
 		public startValue: number,
@@ -13,9 +21,7 @@ class Odometer{
 		//create container
 		$(el).append('<div class="odometer-container"></div>')
 		const container = $(this.el).children('.odometer-container')
-		const scrollDirection = this.startValue > this.endValue ? 'down' : 'up'
-		const increment = scrollDirection === 'down' ? -1 : 1
-		
+
 		//populate digit places
 		for(let i = 0; i < this.startValue.toString().length; i++){
 
@@ -27,15 +33,17 @@ class Odometer{
 			}
 			this.digitPositions.push(0)
 
-			//generate html
-			let firstPos = this.startValue < this.endValue ? this.startValue.toString()[i] : parseInt(this.startValue.toString()[i]) + increment
-			let secondPos = this.startValue > this.endValue ? this.startValue.toString()[i] : parseInt(this.startValue.toString()[i]) + increment
+			//get values to use
+			let firstPos = this.startValue < this.endValue ? this.startValue.toString()[i] : parseInt(this.startValue.toString()[i]) + this.increment
+			let secondPos = this.startValue > this.endValue ? this.startValue.toString()[i] : parseInt(this.startValue.toString()[i]) + this.increment
 
+			//loop back to start
 			if(firstPos > 9) firstPos = 0
 			if(firstPos < 0) firstPos = 9
 			if(secondPos > 9) secondPos = 0
 			if(secondPos < 0) secondPos = 9
 
+			//create html
 			$(container).append(
 				`<div class="digit">
 				<div>${firstPos}</div>
@@ -43,34 +51,31 @@ class Odometer{
 				</div>`
 			)
 		}
+		//init vars that depend on html
 
 		//get height based on font size of digits
-		const height = container.children('.digit')[0].children[0].clientHeight
-		$(container).css('height', height)
+		this.height = container.children('.digit')[0].children[0].clientHeight
+		$(container).css('height', this.height)
 		
-		//move digits to top or bottom depending on direction
-		if(scrollDirection === 'down'){
-			$($(container).children('.digit')).css('top', `${0 - height}`)
+		//set top (resting position) and cutoff (point of reset)
+		this.top = this.scrollDirection === 'down' ? 0 - this.height :  0
+		this.cutoff = this.scrollDirection === 'down' ? 0 : 0 - this.height
+
+		//if we are scrolling down, we need to offset the digits to start
+		if(this.scrollDirection === 'down'){
+			$($(container).children('.digit')).css('top', `${0 - this.height}`)
 		}
 	}
 	async update(){
 		const startTime = new Date().getTime()
 		const container = $(this.el).children('.odometer-container')
-		const height = container.children('.digit')[0].children[0].clientHeight
-		const scrollDirection = this.startValue > this.endValue ? 'down' : 'up'
-		const top = scrollDirection === 'down' ? height - height * 2 : 0
-		const cutoff = scrollDirection === 'down' ? 0 : height - height * 2
-		const digitsToHide = this.startValue.toString().length - this.endValue.toString().length
-		const targetSpeed = 50
 		let speed = 1
-
-
 		let spinning = true
 
 		//init top position for all digits
 		const htmlDigits = $(container).children('.digit')
 		for(let i = 0; i < this.digitPositions.length; i++){
-			$(htmlDigits).css('top', `${this.digitPositions[top]}px`)
+			$(htmlDigits).css('top', `${this.digitPositions[this.top]}px`)
 		}
 
 
@@ -86,12 +91,12 @@ class Odometer{
 			for(let i = 0; i < htmlDigits.length; i++){
 				//reset if needed
 				if(
-					(this.digitPositions[i] < cutoff && scrollDirection === 'up') ||
-					(this.digitPositions[i] > cutoff && scrollDirection === 'down')
+					(this.digitPositions[i] < this.cutoff && this.scrollDirection === 'up') ||
+					(this.digitPositions[i] > this.cutoff && this.scrollDirection === 'down')
 				){
-					this.digitPositions[i] = scrollDirection === 'down' ? 0 - height : 0
+					this.digitPositions[i] = this.scrollDirection === 'down' ? 0 - this.height : 0
 					//increment numbers
-					const increment = scrollDirection === 'down' ? -1 : 1
+					const increment = this.scrollDirection === 'down' ? -1 : 1
 					let newVal = parseInt($($(htmlDigits[i]).children()[0]).html()) + increment
 					if(newVal > 9) newVal = 0
 					if(newVal < 0) newVal = 9
@@ -104,12 +109,12 @@ class Odometer{
 				} 
 				//apply new height
 				$(htmlDigits[i]).css('top', `${this.digitPositions[i]}px`)
-				if(scrollDirection === 'up'){
+				if(this.scrollDirection === 'up'){
 					this.digitPositions[i] -= (speed/10 + 2**i)
 				}else{
 					this.digitPositions[i] += (speed/10 + 2**i)
 				}
-				if(speed < targetSpeed){
+				if(speed < this.targetSpeed){
 					speed+=.5
 				}
 			}
@@ -122,31 +127,31 @@ class Odometer{
 				//move each digit to its reset position
 				const setProperValues = setInterval(() => {
 					let isFinished = true
-					const finalPosition = scrollDirection === 'down' ? cutoff : top
+					const finalPosition = this.scrollDirection === 'down' ? this.cutoff : top
 					for(let i = 0; i < htmlDigits.length; i++){
 						//check if the end result digit is in the proper div
-						const properDigitIsShown = scrollDirection === 'down' ? 
+						const properDigitIsShown = this.scrollDirection === 'down' ? 
 							$($(htmlDigits[i]).children()[0]).html().toString() === this.digits[i].toString() :
 							$($(htmlDigits[i]).children()[1]).html().toString() === this.digits[i].toString()
 
 						//check if the digit place is in the proper position
-						const digitIsInCorrectPosition = scrollDirection === 'down' ? 
-							this.digitPositions[i] >= cutoff : 
-							this.digitPositions[i] <= top
+						const digitIsInCorrectPosition = this.scrollDirection === 'down' ? 
+							this.digitPositions[i] >= this.cutoff : 
+							this.digitPositions[i] <= this.top
 						
 						if(!properDigitIsShown || !digitIsInCorrectPosition){
 							//reset with new number
 							if(
-								(this.digitPositions[i] < cutoff && scrollDirection === 'up') ||
-								(this.digitPositions[i] > cutoff && scrollDirection === 'down')
+								(this.digitPositions[i] < this.cutoff && this.scrollDirection === 'up') ||
+								(this.digitPositions[i] > this.cutoff && this.scrollDirection === 'down')
 							){
 			
-								this.digitPositions[i] = scrollDirection === 'down' ? 0 - height : 0
+								this.digitPositions[i] = this.scrollDirection === 'down' ? 0 - this.height : 0
 
 								//increment numbers
-								const increment = scrollDirection === 'down' ? -1 : 1
+								const increment = this.scrollDirection === 'down' ? -1 : 1
 
-								if(scrollDirection === 'down'){
+								if(this.scrollDirection === 'down'){
 									let newVal = parseInt($($(htmlDigits[i]).children()[1]).html()) + increment
 									if(newVal > 9) newVal = 0
 									if(newVal < 0) newVal = 9
@@ -161,13 +166,13 @@ class Odometer{
 								}
 
 								//set the leading 0s to be opacity 0
-								if(i < digitsToHide && this.digits[i] === 0){
+								if(i < this.digitsToHide && this.digits[i] === 0){
 									$($(htmlDigits[i]).children()[0]).css('opacity', 0)	
 								}
 							} 
 							//apply new height
 							$(htmlDigits[i]).css('top', `${this.digitPositions[i]}px`)
-							if(scrollDirection === 'up'){
+							if(this.scrollDirection === 'up'){
 								this.digitPositions[i] -= (speed/10 + 2**i)
 							}else{
 								this.digitPositions[i] += (speed/10 + 2**i)
